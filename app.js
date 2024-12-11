f6c7669eb0cf0a542eb45e7a6ce6a4a7933fc9f3
Mazino <fredozv01@gmail.com>
2024-12-11 21:27:58 +0100

    slider

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', init)
+
+
+function init() {
+    const animation = gsap.timeline({ repeat: -1 })
+
+    animation
+        .to('.vertical-slider p ', {
+            y: 0, autoAlpha: 1, stagger: 1
+        })
+        .to('.vertical-slider p ', {
+            y: -100, opacity: 0, autoAlpha: 0, stagger: 1
+        }, 1)
+}
